934bf12d3058f7a63e5190a4d6eb71bf4902dc68
Khakachang Tripura <khakachang22@gmail.com>
2026-01-14 23:25:39 +0530

    Added /students endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
+
+
+class Student(BaseModel):
+    name : str
+    rollno : str
+
+
+my_students : list[Student] = [
+    {"name": "khakachang","rollno": "d25cs202"},
+    {"name": "Payel Debbarma","rollno": "d25cs211"}
+]
+
 
 @app.get("/")
 async def root():
-    return {"msg": "Hello World"}
-    
+    return {"msg": "Hello World Updated"}
+
+
+@app.get("/student", response_model=list[Student])
+async def get_students() -> list[Student]:
+    return my_students 
